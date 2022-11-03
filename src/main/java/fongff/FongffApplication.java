256ce10b82812a5109429688063c6bdbbcef5bc3
Paul <paul.lee.2022.09@gmail.com>
2022-11-03 19:47:20 +0800

    fix: add input maxLength 	modified:   src/main/resources/templates/fongff-content.html

--- src/main/java/fongff/FongffApplication.java
+++ src/main/java/fongff/FongffApplication.java
@@ -12,7 +12,6 @@ public class FongffApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FongffApplication.class, args);
-//        test
     }
 
     @Override
